dccea96c238982e13effbaf06d91bcc321419ff5
eniddealla <allaeddinekhalid@gmail.com>
2022-03-14 01:55:51 +0100

    progress

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: akhalid <akhalid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 02:17:12 by akhalid           #+#    #+#             */
-/*   Updated: 2022/03/12 08:53:24 by akhalid          ###   ########.fr       */
+/*   Updated: 2022/03/14 01:52:35 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 int main(int argc, char **argv)
 {
 	if (argc == 1)
-	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-		return (0);
-	}
-	for (int j = 1; j < argc; j++)
-	{
-		for (int i = 0; i < (int)strlen(argv[j]); i++)
+	else
+		for (int j = 1; j < argc; j++)
 		{
-			std::cout << (char)toupper(argv[j][i]);
+			for (int i = 0; i < (int)strlen(argv[j]); i++)
+			{
+				std::cout << (char)toupper(argv[j][i]);
+			}
 		}
-		
-	}	
+	std::cout << std::endl;
 	return (0);
 }
