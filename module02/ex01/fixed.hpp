ad7e3623fc4619b37181153871a95e044fbf613b
eniddealla <allaeddinekhalid@gmail.com>
2022-03-18 16:50:45 +0100

    progress

--- module02/ex01/fixed.hpp
+++ module02/ex01/fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: akhalid <akhalid@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 14:47:51 by akhalid           #+#    #+#             */
-/*   Updated: 2022/03/18 15:38:32 by akhalid          ###   ########.fr       */
+/*   Updated: 2022/03/18 15:54:44 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,26 @@
 # include <iostream>
 
 class Fixed
-{
+{	
 	public:
 		Fixed( void );
+		Fixed( const int number );
+		Fixed( const float number );
 		Fixed( const Fixed& f );
 		Fixed& operator=(const Fixed& f );
 		~Fixed();
+
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
+
+		float toFloat( void ) const;
+		int toInt( void ) const;
 
 	private:
 		int fixedPoint;
 		static const int nFraction = 8;	
 };
 
+std::ostream& operator<<(std::ostream &output, const Fixed& f);
 
 #endif
