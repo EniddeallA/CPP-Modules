6bbd36bc2f89d891dab8438e7e1a0edbce5979a6
eniddealla <allaeddinekhalid@gmail.com>
2022-03-18 15:39:38 +0100

    progress

---
+++ module02/ex00/fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akhalid <akhalid@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 14:47:51 by akhalid           #+#    #+#             */
+/*   Updated: 2022/03/18 15:38:40 by akhalid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+
+class Fixed
+{
+	public:
+		Fixed( void );
+		Fixed( const Fixed& f );
+		Fixed& operator=(const Fixed& f );
+		~Fixed();
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+
+	private:
+		int fixedPoint;
+		static const int nFraction = 8;	
+};
+
+
+#endif
---
+++ module02/ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akhalid <akhalid@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 14:47:55 by akhalid           #+#    #+#             */
+/*   Updated: 2022/03/18 15:17:13 by akhalid          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fixed.hpp"
+
+int main()
+{
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
