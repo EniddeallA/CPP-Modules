af93336a86c9fce3941b9af3b644d47258e4d4df
eniddealla <allaeddinekhalid@gmail.com>
2022-03-31 02:06:25 +0000

    progress

--- module04/ex03/AMateria.hpp
+++ module04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: akhalid <akhalid@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 02:50:30 by akhalid           #+#    #+#             */
-/*   Updated: 2022/03/28 20:35:55 by akhalid          ###   ########.fr       */
+/*   Updated: 2022/03/31 01:12:49 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define AMATERIA_HPP
 
 #include "ICharacter.hpp"
-
+createMateria
 class ICharacter;
 
 class AMateria
