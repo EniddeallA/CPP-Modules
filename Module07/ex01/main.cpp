fc10374c9be0d782f0f721c2f1eda3849009ce18
eniddealla <allaeddinekhalid@gmail.com>
2022-04-10 00:37:45 +0000

    progress

--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -6,29 +6,40 @@
 /*   By: akhalid <akhalid@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 04:13:16 by akhalid           #+#    #+#             */
-/*   Updated: 2022/04/09 04:27:51 by akhalid          ###   ########.fr       */
+/*   Updated: 2022/04/10 00:11:54 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-template<typename T>
-void print(T& e)
+class Awesome
 {
-    std::cout << e << std::endl;
-}
+	public:
+		Awesome( void ) : _n( 42 ) { return; }
+		int get( void ) const { return this->_n; }
+	private:
+		int _n;
+};
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
 
-template<typename T>
-void increment(T& e)
-{
-    e += 1;
+template< typename T >
+void print( T const & x )
+{ 
+	std::cout << x << std::endl;
+	return; 
 }
 
 int main( void )
 {
-	int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-	::iter(arr, 10, increment);
-	::iter(arr, 10, print);
+	int arr[] = {0,1,2,3,4};
+	::iter( arr, 5, print );
+
+	std::cout << std::endl;
+
+	Awesome tab[5];
+	::iter( tab, 5, print );
+
+	std::cout << std::endl;
 	
 	std::string str_arr[3] = {"hello", "world" , "!"};
 	::iter(str_arr, 3, print);
--- Module07/ex02/main.cpp
+++ Module07/ex02/main.cpp
@@ -2,61 +2,6 @@
 #include  <cstdlib>
 #include "Array.hpp"
 
-/*
-
-#define MAX_VAL 750
-int main(void)
-{
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    std::srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = std::rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
-}
-
-*/
-
 int main(void)
 {
     Array<int> arr(6);
@@ -69,7 +14,6 @@ int main(void)
     {
         std::cout << arr[i] << std::endl;
     }
-
     try {
         arr[5] = 123456;
         std::cout << arr[5] << std::endl;
@@ -78,6 +22,8 @@ int main(void)
     {
         std::cout << e.what() << std::endl;
     }
+
+    std::cout << std::endl;
 
     Array<int> arrr;
     arrr = arr;
