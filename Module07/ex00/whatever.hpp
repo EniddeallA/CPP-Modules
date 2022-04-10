2a8a900d065d026deba1ff9ed1494397e24f750a
eniddealla <allaeddinekhalid@gmail.com>
2022-04-10 01:26:45 +0000

    progress

--- Module07/ex00/whatever.hpp
+++ Module07/ex00/whatever.hpp
@@ -6,25 +6,25 @@
 /*   By: akhalid <akhalid@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 03:56:48 by akhalid           #+#    #+#             */
-/*   Updated: 2022/04/09 04:09:36 by akhalid          ###   ########.fr       */
+/*   Updated: 2022/04/10 01:04:34 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 template <class T> 
-T min( T a, T b)
+T const& min( T const &a, T const &b)
 {
 	return ((a < b) ? a : b);
 }
 
 template <class T> 
-T max( T a, T b)
+T const& max( T const &a, T const &b)
 {
 	return ((a > b) ? a : b);
 }
 
 template <class T> 
-void swap( T& a, T& b)
+void swap( T &a, T &b)
 {
 	T c = a;
 	a = b;
--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -6,14 +6,14 @@
 /*   By: akhalid <akhalid@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 04:13:18 by akhalid           #+#    #+#             */
-/*   Updated: 2022/04/10 00:09:46 by akhalid          ###   ########.fr       */
+/*   Updated: 2022/04/10 01:01:36 by akhalid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iostream"
 
 template <class T>
-void iter(T* arr, int length, void (*function)(T const&))
+void iter(T* const arr, int length, void (*function)(T const&))
 {
 	for (int i = 0; i < length; i++)
 		function(arr[i]);
